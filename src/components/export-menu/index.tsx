132e6e2110671d51ea0d07600db6b1acf8968c5a
baraa65 <baraash65@hotmail.com>
2022-10-30 06:35:15 +0200

    add export as yaml

--- src/components/export-menu/index.tsx
+++ src/components/export-menu/index.tsx
@@ -1,11 +1,12 @@
-import { Button, Dialog, Menu, MenuItem } from '@mui/material'
+import { Button, Menu, MenuItem } from '@mui/material'
 import React, { useContext, useState } from 'react'
 import { MainContext } from '../../pages/main/contexts'
 import { downloadFile } from '../../utils/download-file'
+import YAML from 'yaml'
 
 function ExportDialog() {
 	const { notes } = useContext(MainContext)
-	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+	const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
 	const open = Boolean(anchorEl)
 	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
 		setAnchorEl(event.currentTarget)
@@ -19,6 +20,13 @@ function ExportDialog() {
 		const content = 'data:text/json;charset=utf-8,' + encodeURIComponent(json)
 
 		downloadFile('notes.json', content)
+	}
+
+	const exportAsYaml = () => {
+		const yaml = YAML.stringify(notes)
+		const content = 'data:text/yaml;charset=utf-8,' + encodeURIComponent(yaml)
+
+		downloadFile('notes.yaml', content)
 	}
 
 	return (
@@ -37,7 +45,7 @@ function ExportDialog() {
 				<Menu id="basic-menu" anchorEl={anchorEl} open={open} onClose={handleClose}>
 					<MenuItem onClick={exportAsJson}>JSON</MenuItem>
 					<MenuItem onClick={handleClose}>XML</MenuItem>
-					<MenuItem onClick={handleClose}>YAML</MenuItem>
+					<MenuItem onClick={exportAsYaml}>YAML</MenuItem>
 				</Menu>
 			</div>
 		</>
